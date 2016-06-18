649e51391bd4f1b1dbcd84e8915a7a02c7cc1333
Matt Gable <matthewgable@gmail.com>
2016-06-18 05:00:31 -0400

    Added Knuth/Fisher-Yates shuffler

--- Java/JavaAlgorithms/src/com/algorithms/Client.java
+++ Java/JavaAlgorithms/src/com/algorithms/Client.java
@@ -1,6 +1,21 @@
 package com.algorithms;
 
+/**
+ * the client class for the JavaAlgorithms package
+ * @author Matt
+ */
 public class Client 
 {
-
+	/**
+	 * the client class's main method
+	 * @param args the standard array of Strings in the main method
+	 */
+	public static void main (String [] args)
+	{
+		int [] testArray = new int [] {1, 5, 4, 3};
+		
+		KnuthFisherYatesShuffler kfyShuffler= new KnuthFisherYatesShuffler();
+		
+		kfyShuffler.intShuffler(testArray);
+	}
 }
---
+++ Java/JavaAlgorithms/src/com/algorithms/KnuthFisherYatesShuffler.java
@@ -0,0 +1,47 @@
+package com.algorithms;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * an implementation of the Knuth/Fisher-Yates shuffle.
+ * @author Matt
+ */
+public class KnuthFisherYatesShuffler 
+{
+	
+	/**
+	 * performs the Knuth/Fisher-Yates shuffle on an array of integers (O(n)).
+	 * @param inputArray the array of integers to be shuffled.
+	 */
+	public void intShuffler (int [] inputArray)
+	{
+		System.out.println("The inputArray before shuffling is: " + Arrays.toString(inputArray));
+		
+		//Initialize a new instance of Random
+		Random rand = new Random();
+		
+		int n = inputArray.length;
+		
+		for (int i = 0; i < n; i ++)
+		{
+			//Decrement n so we essentially pull from the numbers without replacement 
+			swap(inputArray, i, rand.nextInt(n--));
+		}
+		
+		System.out.println("The inputArray after shuffling is: " + Arrays.toString(inputArray));
+	}
+	
+	/**
+	 * performs an in-place swap of two elements in an array of integers.
+	 * @param inputArray the array that contains the elements to be swapped.
+	 * @param indexA the index of the first element to be swapped.
+	 * @param indexB the index of the second element to be swapped.
+	 */
+	private void swap (int [] inputArray, int indexA, int indexB)
+	{
+		int temp = inputArray[indexA];
+		inputArray[indexA] = inputArray[indexB];
+		inputArray[indexB] = temp;
+	}
+}
