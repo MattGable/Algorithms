d73466cea55266327c7030208433420d8a6687f5
Matt Gable <matthewgable@gmail.com>
2016-07-19 10:53:44 -0400

    Formatting

--- Java/JavaAlgorithms/src/com/dataStructures/BinarySearchTree.java
+++ Java/JavaAlgorithms/src/com/dataStructures/BinarySearchTree.java
@@ -9,6 +9,8 @@ package com.dataStructures;
  * 
  * This implementation uses zero as a "blank" node. It also handles duplicates.
  * 
+ * Currently, this is kept as simple as possible (no input validation, etc.).
+ * 
  * BinarySearchTree and BSTNode are written in response to the 
  * learning problems posed by Nick Parlante here:
  * http://cslibrary.stanford.edu/110/BinaryTrees.html
@@ -17,20 +19,32 @@ package com.dataStructures;
  */
 public class BinarySearchTree
 {
-	//Declare and initialize the root BstNode
+	/**
+	 * The root node is declared and initialized.
+	 */
 	private BstNode root = new BstNode();
 	
+	
+	/**
+	 * Returns the root BstNode object for the binary search tree.
+	 * @return The root node.
+	 */
 	public BstNode getRoot ()
 	{
 		return root;
 	}
 	
 
+	/**
+	 * Inserts a node recursively into the binary search tree. When using this method
+	 * explicitly, pass the node from which to start the insertion from (usually the root). 
+	 * @param inputNode The node examined. If zero, then the value is placed there.
+	 * @param inputValue The value to be placed into the binary search tree.
+	 */
 	public void insertNode (BstNode inputNode, int inputValue)
 	{
 
-		//Base case if a node object is zero
-		//then place the new node addition here
+		//Base case: If a node object is zero, then place the new node addition here.
 		if (inputNode.getValue() == 0)
 		{
 			inputNode.setValue(inputValue);
@@ -38,36 +52,36 @@ public class BinarySearchTree
 			return;
 		}
 		
-		//Check inputValue against current value of node
+		//If the inserted value is less than or equal to the current node.
 		if (inputValue <= inputNode.getValue())
 		{
+			//If the left node is null, initialize it.
 			if (inputNode.getLeftNode() == null)
 			{
 				inputNode.initializeLeftNode();
 			}
 			
 			System.out.println("Moving down the tree to the left (<)...");
+			//Recursively check the next node's value.
 			insertNode (inputNode.getLeftNode(), inputValue);
 		}
-		
+		//If the inserted value is more than the current node.
 		else if (inputValue > inputNode.getValue())
 		{
-			
+			//If the right node is null, initialize it.
 			if (inputNode.getRightNode() == null)
 			{
 				inputNode.initializeRightNode();
 			}
 			
 			System.out.println("Moving down the tree to the right (>)...");
-			insertNode (inputNode.getRightNode(), inputValue);
-			
+			//Recursively check the next node's value.
+			insertNode (inputNode.getRightNode(), inputValue);	
 		}
-		
-		
 	}
 	
 	/**
-	 * print the root node's value
+	 * Prints the root node's integer value to the console. 
 	 */
 	public void printTreeRoot ()
 	{
@@ -78,17 +92,24 @@ public class BinarySearchTree
 
 		else //if (root.getValue() == 0)
 		{
-			System.out.println("Root is null");
+			System.out.println("Root is zero");
 		}
-		
 	}
 	
-	//TODO: Finish implementation here!!
+	/**
+	 * The access call to a recursive size function.
+	 * @return the size(BstNode inputNode) method.
+	 */
 	public int size ()
 	{
 		return size(root);
 	}
 
+	/**
+	 * The recursive size method.
+	 * @param inputNode The node to be examined and counted.
+	 * @return The numeric result of the recursive call to the function.
+	 */
 	private int size (BstNode inputNode)
 	{
 		if (inputNode.getValue() == 0)
@@ -99,23 +120,23 @@ public class BinarySearchTree
 		else if (inputNode.getLeftNode() != null && inputNode.getLeftNode().getValue() != 0 && 
 				 inputNode.getRightNode() != null && inputNode.getRightNode().getValue() != 0)
 		{
-			System.out.println(inputNode.getValue() + "has 2 nonzero children.");
+			System.out.println("'" + inputNode.getValue() + "' has 2 nonzero children.");
 			return size(inputNode.getLeftNode()) + 1 + size(inputNode.getRightNode());
 		}
 		else if (inputNode.getLeftNode() != null && inputNode.getLeftNode().getValue() != 0 && inputNode.getRightNode() == null)
 		{
-			System.out.println(inputNode.getValue() + "has 1 nonzero child to the left (<)");
+			System.out.println("'" + inputNode.getValue() + "' has 1 nonzero child to the left (<)");
 			return size(inputNode.getLeftNode()) + 1;
 		}
 		
 		else if(inputNode.getRightNode() != null && inputNode.getRightNode().getValue() != 0 && inputNode.getLeftNode() == null)
 		{
-			System.out.println(inputNode.getValue() + "has 1 nonzero child to the right (>)");
+			System.out.println("'" + inputNode.getValue() + "' has 1 nonzero child to the right (>)");
 			return size(inputNode.getRightNode()) + 1;
 		}
 		else if (inputNode.getLeftNode() == null && inputNode.getRightNode() == null)
 		{
-			System.out.println(inputNode.getValue() + "has no children.");
+			System.out.println("'" + inputNode.getValue() + "' has no children.");
 			return 1;
 		}
 		else 
@@ -127,6 +148,10 @@ public class BinarySearchTree
 	
 	
 	//TODO: complete this implementation
+	/**
+	 * @param lookupValue
+	 * @return
+	 */
 	public boolean lookup (int lookupValue)
 	{
 		BstNode currentNode = root;
--- Java/JavaAlgorithms/src/com/dataStructures/BstNode.java
+++ Java/JavaAlgorithms/src/com/dataStructures/BstNode.java
@@ -1,47 +1,88 @@
 package com.dataStructures;
 
+/**
+ * This class represents a node for use in a binary search tree. For testing purposes, many of the 
+ * methods are public.
+ * @author Matt
+ */
 public class BstNode
 {
+	/**
+	 * Nodes' values are always initialized to zero.
+	 */
 	private int value = 0;
 	
+	/**
+	 * Left and right nodes are not automatically initialized--to save space.
+	 */
 	private BstNode leftNode;
 	private BstNode rightNode;
 	
-	public void setValue (int inputValue)
+	/**
+	 * Allows the value of the node to be set to any integer value.
+	 * @param inputValue The value to assign to the node.
+	 */
+	protected void setValue (int inputValue)
 	{
 		value = inputValue;
 	}
 	
+	/**
+	 * Allows the value of the node to be retrieved.
+	 * @return The value currently held in the node.
+	 */
 	public int getValue ()
 	{
 		return value;
 	}
 	
+	/**
+	 * Allows the value of the node's left child to be retrieved.
+	 * @return The value currently held in the node's left child.
+	 */
 	public int getLeftValue ()
 	{
 		return leftNode.getValue();
 	}
 	
+	/**
+	 * Allows the value of the node's right child to be retrieved.
+	 * @return The value currently held in the node's right child.
+	 */
 	public int getRightValue ()
 	{
 		return rightNode.getValue();
 	}
 	
-	public void initializeLeftNode()
+	/**
+	 * Initializes the left node reference in the node.
+	 */
+	protected void initializeLeftNode()
 	{
 		leftNode = new BstNode();
 	}
 	
-	public void initializeRightNode()
+	/**
+	 * Initializes the right node reference in the node.
+	 */
+	protected void initializeRightNode()
 	{
 		rightNode = new BstNode();
 	}
 	
+	/**
+	 * Allows the the node's left child reference to be retrieved.
+	 * @return A reference to the node's left child.
+	 */
 	public BstNode getLeftNode ()
 	{
 		return leftNode;
 	}
 	
+	/**
+	 * Allows the the node's right child reference to be retrieved.
+	 * @return A reference to the node's right child.
+	 */
 	public BstNode getRightNode ()
 	{
 		return rightNode;
